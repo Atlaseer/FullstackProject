53ecbb1e485102cb7c4f82c5a274d43a9cb4a1c8
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-05-08 13:40:27 +0200

    Update PostCard.jsx
    Added link to posts

--- client/src/Components/PostCard.jsx
+++ client/src/Components/PostCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/Main.css';
+import { Link } from 'react-router-dom';
 
 const PostCard = ({ post }) => {
   return (
     <div className="post-card">
-      <h3>{post.title}</h3>
+      <h3> <Link to={`/post/${post.id}`}>{post.title} </Link></h3>
       <div className="post-meta">
         <span>By {post.author}</span>
-        <span>{post.replies} replies</span>
+        <span>{post.replies || 0} replies</span>
       </div>
     </div>
   );
