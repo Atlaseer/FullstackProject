cd49715f227eb71f60ed69a5de516becf1d84d48
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-05-08 15:07:51 +0200

    Update ViewPostPage.jsx

--- client/src/Pages/ViewPostPage.jsx
+++ client/src/Pages/ViewPostPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { usePosts } from "../contexts/PostContext";
+
 
 const ViewPostPage = () => {
     const { id } = useParams();
-    const { posts } = usePosts();
-    const post = posts.find((post) => post.id === parseInt(id, 10));
+
+    // const post = posts.find((post) => post.id === parseInt(id, 10));
 
     if(!post) {
         return <div>Post not found</div>;
