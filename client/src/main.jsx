1465092615e6156db937b4687a9140143fa53d8c
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-05-08 13:31:19 +0200

    Added post context to store posts for now
    Also wrapped App in main.jsx around it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx' 
+import { PostProvider } from './contexts/PostContext.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <PostProvider>
+        <App />
+      </PostProvider>
     </AuthProvider>
   </StrictMode>,
 )
