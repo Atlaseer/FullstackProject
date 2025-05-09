4d1ababb1403eaea1fe51f7fb0d81b0147825c30
Simon Persson <atlaspersson@outlook.com>
2025-05-09 16:11:42 +0200

    Created Post.js model, working on posts.js route

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
-import User from "./User.js";
-//import { Comment } from "./Comment.js";
+import mongoose from 'mongoose';
+import User from './User.js';
+import Comment from './Comment.js';
+
 //import { Category } from "./Category.js";
 //import { Tag } from "./Tag.js";
 
-const postSchema = new mongoose.Schema(
-  {
+const postSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now },
     title: { type: String, required: true },
     content: { type: String, required: true },
-    tags: { type: [String], ref: "Tag" },
-    categories: { type: [String], ref: "Category" },
+    tags: { type: [String], ref: 'Tag' },
+    categories: { type: [String], ref: 'Category' },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  },
+  { timestamps: true }
+);
 
-    }
-    );
+const Post = mongoose.model('Post', postSchema);
+export default Post;
