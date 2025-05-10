233befdae6199830212ed4096be04235cc1abc36
Simon Persson <atlaspersson@outlook.com>
2025-05-10 15:54:33 +0200

    Created post.js route to get all, get by ID and update

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,6 +3,7 @@ import Post from '.models/Post.js'
 
 const router = express.Router();
 
+//Creates new post
 router.post('/', async (req, res) => {
     try {
         const {user, title, content, tags, categories } = req.body;
@@ -14,4 +15,42 @@ router.post('/', async (req, res) => {
     }
 })
 
-//router.get('/', )
+//Gets all posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find().populate('user', 'username').populate('comments');
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+//Gets post by ID
+router. get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id).populate('user', 'username').populate('comments')
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+        res.status(200).json(post);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message})
+    }
+})
+
+//Updates post by ID
+router.put('/:id', async (req, res) => {
+    try {
+        const { title, content, tags, categories } = req.body;
+        const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id,
+            { title, content, tags, categories },
+            { new: true }
+        );
+        if (!updatedPost) return res.status(404).json ({ message: 'Post not found'});
+        res.status(200).json(updatedPost);
+    
+        } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
