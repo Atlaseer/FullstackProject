503f7c6b4dd97b85b4c595357ab219f8f78e3042
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-05-11 00:11:15 +0200

    Fixed some more styling issues also linked new post to website

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -12,25 +12,33 @@ const Header = () => {
 
     return(
         <header className='forum-header'>
-            <div className='forum-logo'><Link to="/"><h2>FoodLovers</h2></Link></div>
+            <div className='forum-seperate-part'>
+                <div className='forum-logo'><Link to="/"><h2>FoodLovers</h2></Link></div>
 
-            <div className="forum-search">
-                <FaSearch className="search-icon" />
-                <input
-                  type="text"
-                  placeholder="Search topics..."
-                  className="search-input"
-                />
+                <div className="forum-search">
+                    <FaSearch className="search-icon" />
+                    <input
+                      type="text"
+                      placeholder="Search topics..."
+                      className="search-input"
+                      />
+                </div>
             </div>
+            <div className='forum-seperate-part'>
+
             <ThemeToggle/>
             <Navbar/>
+            </div>
             <div className="forum-user">
                 {user ? (
+                    <div>
+                     <Link to="/newpost" className="auth-link">Create</Link>
                     <UserMenu/>
+                    </div>
                 ) : 
                     <div className="auth-buttons">
                         <Link to="/login" className="auth-link">Sign In</Link>
-                        <Link to="/signup" className="auth-link">Register</Link>
+                        <Link to="/signup" className="auth-link">Sign Up</Link>
                     </div>
                 }
             </div>
