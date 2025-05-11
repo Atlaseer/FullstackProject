4d1a77e3a7287fc8c4b6e5da673b1ed525039b9a
liconiferous <147435606+liconiferous@users.noreply.github.com>
2025-05-11 23:18:06 +0200

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -6,26 +6,27 @@ import Comment from './Comment.js';
 //import Tag from "./Tag.js";
 
 const postSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    createdAt: { type: Date, default: Date.now },
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    tags: { type: [String], ref: 'Tag' },
-    categories: { type: [String], ref: 'Category' },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-    ratings: [
-      { 
-        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        rating: { type: Number, min: 1, max: 5 } 
-      }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  coverImage: { type: String, default: null },
+  tags: { type: [String], ref: 'Tag' },
+  categories: { type: [String], ref: 'Category' },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  ratings: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      rating: { type: Number, min: 1, max: 5 }
+    }],
 
-    averageRating: { type: Number, default: 0 },
-    totalRatings: { type: Number, default: 0 },
-    totalComments: { type: Number, default: 0 },
-    totalLikes: { type: Number, default: 0 },
-    views: { type: Number, default: 0 }
+  averageRating: { type: Number, default: 0 },
+  totalRatings: { type: Number, default: 0 },
+  totalComments: { type: Number, default: 0 },
+  totalLikes: { type: Number, default: 0 },
+  views: { type: Number, default: 0 }
 
-  },
+},
 
   { timestamps: true }
 );
