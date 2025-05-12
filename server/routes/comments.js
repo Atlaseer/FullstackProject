ef0d829c9d1401de17854b56e38b5b272bbf7a3e
Simon Persson <atlaspersson@outlook.com>
2025-05-12 11:42:30 +0200

    Created comments.js route

---
+++ server/routes/comments.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import Comment from '../models/Comment.js';
+import Post from '../models/Post.js';
+import User from '../models/User.js';
+import { verifyToken } from '../middleware/auth.js';
+
+const router = express.Router();
+
+//Create new comment
+router.post('/', verifyToken, async (req, res)=> {
+    try {
+        const { postId, content } = req.body;
+
+        //Checks required fields
+        if(!postId || !content) {
+            return res.status(500).json({ error: 'Post ID and content is required'})
+        }
+
+        const post = await Post.findById(postId);
+        if (!post){
+            return res.status(404).json({ error: 'Post not found'})
+        }
+
+        //Create and save new comment, uses UserId from JWT token
+        const newComment = new Comment({
+            post: postId,
+            user: req.user.id,
+            content,
+        });
+
+        await newComment.save();
+
+        post.totalComments = (post.totalComments || 0) +1
+        await post.save();
+
+        res.status(201).json(newComment)
+    }catch (error) {
+        res.status(500).json({ error: 'Server Error: ${error.message}' });
+    }
+})
